54136075116fa4582d7b4b9f3159a66d5cb11a48
Frankyston Lins <frankyston@progshowzim.local>
2022-02-27 10:26:38 -0300

    Fix test Rspec users_spec.rb

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Users API', type: :request do
   let(:user) { build(:user) }
   let(:headers) { valid_headers.except('Authorization') }
   let(:valid_attributes) do
-    attributes_for(:user, password_confirmation: user.password)
+    attributes_for(:user, password: user.password, password_confirmation: user.password)
   end
 
   # User signup test suite
@@ -35,7 +35,8 @@ RSpec.describe 'Users API', type: :request do
       end
 
       it 'returns failure message' do
-        expect(json['message']).to eq("Validation failed: Password confirmation doesn't match Password")
+        expect(json['message'])
+          .to match(/Validation failed: Password can't be blank, Name can't be blank, Email can't be blank, Password digest can't be blank/)
       end
     end
   end
