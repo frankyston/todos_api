07f49624573c0f2c8db533009247328990a121b0
Frankyston Lins <frankyston@progshowzim.local>
2022-02-27 00:52:24 -0300

    Created Authorize API Request

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,7 +21,8 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   config.include FactoryBot::Syntax::Methods
-  config.include RequestSpecHelper, type: :request
+  config.include RequestSpecHelper
+  config.include ControllerSpecHelper
 
   # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
   config.before(:suite) do
